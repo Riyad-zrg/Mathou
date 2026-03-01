2cee3c7b37b10ca7102cbe121ddab369be57d5ca
Riyad Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-03-01 17:08:12 +0100

    :twisted_rightwards_arrows: Math-3 dockerization
    * :sparkles:  MATH-3 basic application dockerization
    
    * :truck: MATH-3 transform project into monorepo
    
    * :sparkles: MATH-3 add api folder for nestJS backend
    
    * :truck: MATH-3 renamed api folder to backend
    
    * :truck: MATH-3 moved docker compose file to be shared by both apps
    
    * :art: MATH-3 correct stage name
    
    * :bug: MATH-3 fix volume issue
    
    * :fire: MATH-3 remove unneccesary database from project
    
    ---------
    
    Co-authored-by: Zerrougui <riyadzerrougui@gmail.com>

--- prisma/migrations/20260215092518_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Problem" (
-    "id" SERIAL NOT NULL,
-    "statement" TEXT NOT NULL,
-    "correctAnswer" TEXT NOT NULL,
-
-    CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
-);
