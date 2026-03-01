af8c6c49420b119bd4f8b0608a385003c96f3ad6
Riyad Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-03-01 17:02:33 +0100

    :fire: MATH-3 remove unneccesary database from project

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,14 +1,5 @@
-import prisma from "@/lib/prisma";
-
 export default async function Home() {
-  const problems = await prisma.problem.findMany()
   return (
-    <div>
-      <ul>
-        {problems.map((problem) => (
-          <li key={problem.id}>{problem.statement}</li>
-        ))}
-      </ul>
-    </div>
+    <p>Salut</p>
   );
 }
