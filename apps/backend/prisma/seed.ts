08abe117374baddaf098a9101a9cb903c911528f
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-11 21:23:48 +0200

    :sparkles: MATH-10 add password hashing in db seeding config

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -2,20 +2,26 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../src/generated/prisma/client.js";
 import "dotenv/config"
 import { Pool } from "pg";
+import bcrypt from "bcrypt";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 async function main(){
+    const saltRounds = 10;
+    const myPlaintextPassword = 'johndoeaa'
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(myPlaintextPassword, salt); 
+
     const john = await prisma.user.upsert({
         where: { email: "john.doe@gmail.fr"},
         update: {},
         create: {
             email: "john.doe@gmail.fr",
-            firstname: "John",
+            firstname: "Johnaaa",
             lastname: "Doe",
-            password:"johndoe",
+            password:hash,
         },
     });
     console.log(john);
