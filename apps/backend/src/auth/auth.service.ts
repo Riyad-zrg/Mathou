8fdc30c456dc28ffca0935cbace68c3bda116185
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-12 09:47:44 +0200

    🚧 MATH-10 implementation of sign in endpoint

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service.js';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService : UserService){}
+
+    async signIn(email: string, password: string): Promise<any> { /** Ici le type est Promise<any>, il serait plus propre d'utiliser un DTO UserWithoutPassword */
+        const user = await this.userService.findUser({email: email});
+        
+    }
+}
