f17b488b2f9ef3d07675e53fb3b4f2a153c3d578
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-14 19:40:05 +0200

    :bug: MATH-10 correct auth module import

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module.js';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module.js';
 
 @Module({
   imports: [ConfigModule.forRoot(), PrismaModule, AuthModule],
