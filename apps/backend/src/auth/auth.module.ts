996f1d77ecc4580822ba9f0cc50185ed12eca04e
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-15 12:43:42 +0200

    🔐 MATH-10 add JWT secret signature key in authModule

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { UserModule } from 'src/user/users.module.js';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[UserModule],
+  imports:[UserModule, JwtModule.register({ secret: process.env.JWTSecret})],
   controllers: [AuthController],
   providers: [AuthService],
 })
