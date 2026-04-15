75ab5238d9761fad4fe5f54ef5359769f421aa4b
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-15 11:33:44 +0200

    :sparkles: MATH-10 update auth service to compare with bcrypt

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,12 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service.js';
+import bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
     constructor(private userService : UserService){}
 
-    async signIn(email: string, password: string): Promise<any> { /** Ici le type est Promise<any>, il serait plus propre d'utiliser un DTO UserWithoutPassword */
+    async signIn(email: string, incomingPassword: string): Promise<any> { /** Ici le type est Promise<any>, il serait plus propre d'utiliser un DTO UserWithoutPassword */
         const user = await this.userService.findUser({email: email});
-        
+        const match = await bcrypt.compare(incomingPassword, user.password);
+
+        if(!match){
+            throw new UnauthorizedException();
+        }
+
+        const {password, ...userWithoutPassword} = user;
+
+        return userWithoutPassword;
     }
 }
