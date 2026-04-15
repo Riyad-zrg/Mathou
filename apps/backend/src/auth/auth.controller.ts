45af67c7722861eb3ea6a473f2f20dd378256535
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-15 12:06:00 +0200

    :sparkles: MATH-10 create sign in dto and use it in auth controller sign in endpoint

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, HttpStatus, Body, HttpCode } from '@nestjs/common';
+import { AuthService } from './auth.service.js';
+import { SignInDto } from './dto/sign-in.dto.js';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    signIn(@Body() signInDto: SignInDto) {
+        return this.authService.signIn(signInDto.email, signInDto.password);
+    }
+}
---
+++ apps/backend/src/auth/dto/sign-in.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class SignInDto {
+    
+    @IsEmail()
+    readonly email!: string;
+
+    @IsString()
+    @MinLength(10)
+    readonly password!: string;
+}
