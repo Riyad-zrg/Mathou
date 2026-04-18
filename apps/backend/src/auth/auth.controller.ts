ed3cd8fd2fa317bb25b7c7a83063df479f90f958
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-18 16:11:41 +0200

    🔥 MATH-10 remove unneccesary decorator

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -13,7 +13,6 @@ export class AuthController {
         return this.authService.signIn(signInDto.email, signInDto.password);
     }
 
-    @UseGuards(AuthGuard)
     @Get('profile')
     getProfile(@Request() req:any){
         return req.user;
