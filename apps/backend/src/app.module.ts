fa1c24e0e82e8dcc22cce1a4a12ca7cdcd0ac610
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-20 09:48:29 +0200

    :sparkles: MATH-11 create logger middleware to get info about http requests

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { LoggerMiddleware } from './middleware/logger.middleware.js';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
