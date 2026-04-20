fb585978d22b7ff5314cd8de608eb7272351c503
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-20 21:16:07 +0200

    :sparkles: MATH-12 enable cookie middleware in app and fix cors problematic

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({origin:'http://localhost:3000', credentials:true});
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
