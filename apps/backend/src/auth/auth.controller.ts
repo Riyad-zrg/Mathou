505e9432e730b87cf7f128ceb8060d0ca45df80b
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-20 09:02:16 +0200

    :bug: MATH-11 set login route to public

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, HttpStatus, Body, HttpCode, UseGuards, Get, Request } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { SignInDto } from './dto/sign-in.dto.js';
+import { Public } from '../common/decorators/public.decorator.js';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @HttpCode(HttpStatus.OK)
+    @Public()
     @Post('login')
     signIn(@Body() signInDto: SignInDto) {
         return this.authService.signIn(signInDto.email, signInDto.password);
