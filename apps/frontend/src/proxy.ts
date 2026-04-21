f14fcf4caa150672da60278a7e47994f00663c91
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-21 09:54:44 +0200

    :sparkles: MATH-11 create proxy file to protect private routes

---
+++ apps/frontend/src/proxy.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const PUBLIC_ROUTES = ["/login", "/signup"];
+
+export function proxy(request: NextRequest) {
+    console.log(request.nextUrl, request.url);
+
+    const { pathname } = request.nextUrl;
+    const token = request.cookies.get("access_token")?.value;
+    if (PUBLIC_ROUTES.includes(pathname)) {
+    return NextResponse.next();
+    }
+    if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
