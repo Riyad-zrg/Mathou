7437b6c5a2d364722c9170bd108d3a225c1752b7
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-22 12:07:29 +0200

    :bug: MATH-11 correct imports

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
-import { UserModule } from '../user/users.module.js';
+import { UserModule } from '../user/user.module.js';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard.js';
