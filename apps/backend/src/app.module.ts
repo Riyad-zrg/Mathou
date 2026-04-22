249d571f9c6b077f116283c4c27399e482c3de32
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-22 12:06:58 +0200

    :sparkles: MATH-11 import user module in app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { LoggerMiddleware } from './middleware/logger.middleware.js';
+import { UserModule } from './user/user.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule, AuthModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
