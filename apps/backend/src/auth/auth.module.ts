6e882e55a1ba7cd23d34cdc5ef5e061d1a27c9e6
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-25 18:35:59 +0200

    :sparkles: MATH-13 import MailerModule in AuthModule

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from '../user/user.module.js';
 import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth.guard.js';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports:[UserModule, JwtModule.register({ global: true, secret: process.env.JWT_SECRET, signOptions: {expiresIn: '60s'}})],
+  imports:[UserModule, JwtModule.register({ global: true, secret: process.env.JWT_SECRET, signOptions: {expiresIn: '60s'}}), MailerModule],
   controllers: [AuthController],
   providers: [AuthService, {provide: APP_GUARD, useClass: AuthGuard}],
 })
