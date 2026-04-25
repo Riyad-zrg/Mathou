d675e1a24160db3648a7497ec9d0c27864dcdc53
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-25 12:22:00 +0200

    :sparkles: MATH-13 modify redirect link after signup form submission to include user's email as a query string parameter

--- apps/frontend/src/services/auth.service.ts
+++ apps/frontend/src/services/auth.service.ts
@@ -39,6 +39,6 @@ export async function signUp(data: any){
         }catch(error: any){
             console.error(error.message);
         } finally {
-            redirect('/signup/verify');
+            redirect(`/signup/verify?email=${data.email}`);
     }
 }
