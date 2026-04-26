1c0f56842c8088a9a27f6e55df380523e5a4de01
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-26 06:48:14 +0200

    🏗️ MATH-13 modify name of check email page to be more congruent

--- apps/frontend/src/proxy.ts
+++ apps/frontend/src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_ROUTES = ["/login", "/signup", "/signup/verify"];
+const PUBLIC_ROUTES = ["/login", "/signup", "/signup/verify", '/signup/check-email'];
 
 export function proxy(request: NextRequest) {
     const { pathname } = request.nextUrl;
