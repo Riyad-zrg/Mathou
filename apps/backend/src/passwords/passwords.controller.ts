504516d41182949cd3199d62a51516005fd37f79
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-28 12:58:20 +0200

    :art: MATH-14 modify password reset route name

--- apps/backend/src/passwords/passwords.controller.ts
+++ apps/backend/src/passwords/passwords.controller.ts
@@ -3,12 +3,12 @@ import { PasswordsService } from './passwords.service.js';
 import { Public } from '../common/decorators/public.decorator.js';
 import { passwordResetSpecifyEmailDto } from '../auth/dto/password-reset-specify-email.dto.js';
 
-@Controller('passwords')
+@Controller('password-reset')
 export class PasswordsController {
   constructor(private readonly passwordsService: PasswordsService) {}
 
   @Public()
-  @Post('password-reset/verify-email')
+  @Post('/verify-email')
   passwordResetSpecifyEmail(@Body() passwordResetSpecifyEmail:passwordResetSpecifyEmailDto)
   {
       return this.passwordsService.passwordResetCheckEmail(passwordResetSpecifyEmail.email);
