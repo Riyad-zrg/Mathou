c036eaf19e60f75173d629d6c7a160bcb3a67b8d
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-28 10:06:25 +0200

    :sparkles: MATH-14 import password module in user module

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UserService } from "./user.service.js";
 import { PrismaModule } from "../prisma/prisma.module.js";
 import { UserController } from './user.controller.js';
+import { PasswordsModule } from "src/passwords/passwords.module.js";
 
 @Module({
-    imports:[PrismaModule],
+    imports:[PrismaModule, PasswordsModule],
     providers:[UserService],
     exports:[UserService],
     controllers: [UserController],
