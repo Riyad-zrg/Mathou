79f3beaa394d2328e602ce5007bae9792a0af853
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-04-28 19:09:27 +0200

    :sparkles: MATH-14 add service function to verify password reset token

--- apps/frontend/src/services/password-reset.service.ts
+++ apps/frontend/src/services/password-reset.service.ts
@@ -20,6 +20,32 @@ export async function passwordResetCheckEmail(data: any){
             return error.message
         } finally {
             if(!isError){
+                redirect(`http://localhost:3000/password-reset/check-email?email=${data.email}`)
+            }
+    }
+}
+
+export async function verifyPasswordResetToken(token:string|null){
+    let isError = false;
+    try{
+        const response = await fetch(`http://localhost:4000/password-reset/verify-password-token`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({token})
+        })
+
+        if (!response.ok) {
+            const result = await response.json()
+            throw new Error(result.message);
+        }
+        }catch(error: any){
+            isError=true;
+            return error.message
+        } finally {
+            if(!isError){
+                redirect(`http://localhost:3000/password-reset/check-email?email=${data.email}`)
             }
     }
 }
