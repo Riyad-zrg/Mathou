89bf9a373797599b1e4e16459c2c727259d6da9d
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-05-31 15:17:17 +0200

    🎨 MATH-14 remove unused function

--- apps/frontend/src/services/password-reset.service.ts
+++ apps/frontend/src/services/password-reset.service.ts
@@ -25,31 +25,6 @@ export async function passwordResetCheckEmail(data: any){
     }
 }
 
-export async function verifyPasswordResetToken(token:string|null){
-    let isError = false;
-    try{
-        const response = await fetch(`http://localhost:4000/password-reset/verify-password-token`,{
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({token})
-        })
-
-        if (!response.ok) {
-            const result = await response.json()
-            throw new Error(result.message);
-        }
-        }catch(error: any){
-            isError=true;
-            return error.message
-        } finally {
-            if(!isError){
-                redirect(`http://localhost:3000/password-reset/check-email?email=${data.email}`)
-            }
-    }
-}
-
 export async function resetChoosePassword(token:string|null, resetId:string|null ,data:any){
     let isError=false;
     try{
