4b788f38bb3efe5351a5d12b6fdbae81eb79cc6e
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-07 10:40:15 +0200

    :sparkles: MATH-17 verification and display of the correct answer after user submission

--- apps/frontend/src/components/ui/maths/AnswersGroup.tsx
+++ apps/frontend/src/components/ui/maths/AnswersGroup.tsx
@@ -1,23 +1,42 @@
 import { shuffle } from "@/src/lib/utils";
 import QuestionAnswer from "./QuestionAnswer";
+import { useEffect, useState } from "react";
 
 export default function AnswersGroup({ result }: { result: number }) {
-  const resultList = [result];
-  for (let i = 0; i < 3; i++) {
-    let value = Math.floor(Math.random() * 100);
+  const [hasUserAnswered, setHasUserAnswered] = useState(false);
+  const [resultList, setResultList] = useState<number[]>([]);
 
-    while (value in resultList) {
-      value = Math.floor(Math.random() * 100);
+  const handleClickOnAnswer = () => {
+    setHasUserAnswered(true);
+  };
+
+  useEffect(() => {
+    const resultList = [result];
+    for (let i = 0; i < 3; i++) {
+      let value = Math.floor(Math.random() * 100);
+
+      while (value in resultList) {
+        value = Math.floor(Math.random() * 100);
+      }
+
+      resultList.push(value);
+
+      shuffle(resultList);
     }
 
-    resultList.push(value);
-  }
+    setResultList(resultList);
+  }, []);
 
-  shuffle(resultList);
   return (
     <div className="grid grid-cols-2 gap-5">
       {resultList.map((answerValue, id) => (
-        <QuestionAnswer value={answerValue} key={id} />
+        <QuestionAnswer
+          value={answerValue}
+          isCorrectAnswer={answerValue === result}
+          handleClickOnAnswer={handleClickOnAnswer}
+          hasUserAnswered={hasUserAnswered}
+          key={id}
+        />
       ))}
     </div>
   );
--- apps/frontend/src/components/ui/maths/QuestionAnswer.tsx
+++ apps/frontend/src/components/ui/maths/QuestionAnswer.tsx
@@ -1,8 +1,29 @@
 import { Card, CardContent } from "../card";
 
-export default function QuestionAnswer({ value }: { value: number }) {
+interface QuestionAnswerProps {
+  value: number;
+  isCorrectAnswer: boolean;
+  handleClickOnAnswer: () => void;
+  hasUserAnswered: boolean;
+}
+export default function QuestionAnswer({
+  value,
+  isCorrectAnswer,
+  handleClickOnAnswer,
+  hasUserAnswered,
+}: QuestionAnswerProps) {
+  if (hasUserAnswered && isCorrectAnswer) {
+    return (
+      <Card
+        className="w-full max-w-sm bg-green-300"
+        onClick={handleClickOnAnswer}
+      >
+        <CardContent>{value}</CardContent>
+      </Card>
+    );
+  }
   return (
-    <Card className="w-full max-w-sm">
+    <Card className="w-full max-w-sm" onClick={handleClickOnAnswer}>
       <CardContent>{value}</CardContent>
     </Card>
   );
