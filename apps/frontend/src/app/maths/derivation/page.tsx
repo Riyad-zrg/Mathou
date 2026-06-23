b520da7b0ed3f17e34136ce0aade4afd109a3682
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-23 09:53:05 +0200

    🏗️ MATH-20 modified import due to a folder name change

--- apps/frontend/src/app/maths/derivation/page.tsx
+++ apps/frontend/src/app/maths/derivation/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import DerivationScreen from "@/src/components/ui/maths/Derivation/DerivationScreen";
+import DerivationScreen from "@/src/components/ui/maths/derivation/DerivationScreen";
 
 export default function DerivationPage() {
   return <DerivationScreen></DerivationScreen>;
