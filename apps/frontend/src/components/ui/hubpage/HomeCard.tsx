4702b17ff3bb24009254c0c31b649c5d2a4eb9b1
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-24 17:05:15 +0200

    :lipstick: MATH-24 modify hubPage cards color and add variations on hover and pressed states

--- apps/frontend/src/components/ui/hubpage/HomeCard.tsx
+++ apps/frontend/src/components/ui/hubpage/HomeCard.tsx
@@ -6,10 +6,12 @@ interface HomeCard {
 }
 export default function HomeCard({ icon, label }: HomeCard) {
   return (
-    <Card className="homeCard">
+    <Card className="bg-orange-500 m-1 hover:bg-orange-600 hover:m-0 active:bg-orange-800 active:m-2 text-white homeCard gap-8">
       <CardHeader></CardHeader>
-      <CardContent className="flex justify-center">{icon}</CardContent>
-      <CardFooter className="flex-col gap-2">{label}</CardFooter>
+      <CardContent className="flex justify-center pb-">{icon}</CardContent>
+      <CardFooter className="flex-col gap-2 text-2xl font-semibold">
+        {label}
+      </CardFooter>
     </Card>
   );
 }
