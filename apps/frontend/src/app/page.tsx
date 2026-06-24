f3ea18084d044e0471232227b112f2afa1e3d520
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-24 16:55:25 +0200

    :lipstick: MATH-24 improve hubpage UI, add header and footer and use HomeCard component

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,5 +1,17 @@
 import { cookies } from "next/headers";
 import { getJWTpayload } from "../lib/utils";
+import { Button } from "../components/ui/button";
+import {
+  Diff,
+  Equal,
+  LogOut,
+  Parentheses,
+  Plus,
+  SquareDivide,
+  Calculator,
+  Grid2x2,
+} from "lucide-react";
+import HomeCard from "../components/ui/hubpage/HomeCard";
 
 export default async function Home() {
   const cookieStore = await cookies();
@@ -7,21 +19,37 @@ export default async function Home() {
   const jwt = accessToken?.value;
   const jwtPayload = await getJWTpayload(jwt);
   return (
-    <div>
+    <div className="flex flex-1 flex-col h-screen">
       {/* <p>Bienvenue {jwtPayload.firstname}! Tu es sur la hub page !</p> */}
-      <a href="/maths/addition">Additions</a>
-      <br />
-      <a href="/maths/fraction">Fractions</a>
-      <br />
-      <a href="/maths/derivation">Dérivations</a>
-      <br />
-      <a href="/maths/factorization">Factorisations</a>
-      <br />
-      <a href="/maths/priorities">Priorités opératoires</a>
-      <br />
-      <a href="/maths/equation">Équations</a>
-      <br />
-      <a href="/maths/proportionality">Proportionnalité</a>
+      <section className="flex grow-0 bg-purple-500 justify-center p-3 header">
+        <p className="text-white text-3xl font-semibold font-sans">SOCATOA</p>
+        <span className="absolute flex w-full justify-end pr-3">
+          <Button
+            variant="outline"
+            size="icon"
+            className="absolute bg-red-500 border-2 border-purple-900 text-white"
+            color="red"
+          >
+            <LogOut />
+          </Button>
+        </span>
+      </section>
+
+      <section className="grid grid-cols-3 grid-rows-3 grow p-5 gap-5 justify-center">
+        <HomeCard icon={<Plus size={60} />} label={"Additions"} />
+        <HomeCard icon={<SquareDivide size={60} />} label={"Fractions"} />
+        <HomeCard icon={<Diff size={60} />} label={"Priorités opératoires"} />
+        <HomeCard icon={<Parentheses size={60} />} label={"Dérivations"} />
+        <HomeCard icon={<Equal size={60} />} label={"Équations"} />
+        <HomeCard icon={<Calculator size={60} />} label={"Factorisations"} />
+        <HomeCard icon={<Grid2x2 size={60} />} label={"Proportionnalité"} />
+      </section>
+
+      <section className="flex flex-1 grow-0 bg-purple-500 justify-center p-3 header">
+        <p className="text-white text-xs font-light font-sans">
+          2026 SOCATOA, Tous droits réservés
+        </p>
+      </section>
     </div>
   );
 }
