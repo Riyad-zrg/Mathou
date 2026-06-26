52cacd2163aebf22ec4ff0d43fa5ca1a01e76e99
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-26 08:27:02 +0200

    :sparkles: MATH-24 redirect to login page after logout

--- apps/frontend/src/components/ui/hubpage/LogOutButton.tsx
+++ apps/frontend/src/components/ui/hubpage/LogOutButton.tsx
@@ -2,10 +2,12 @@
 import { LogOut } from "lucide-react";
 import { Button } from "../button";
 import { logout } from "@/src/services/auth.service";
+import { redirect, RedirectType } from "next/navigation";
 
 export default function LogOutButton() {
   const onLogout = async () => {
     await logout();
+    redirect("/login", RedirectType.replace);
   };
   return (
     <Button
