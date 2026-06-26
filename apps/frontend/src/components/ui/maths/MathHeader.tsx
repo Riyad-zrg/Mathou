6eecd4858e79409cb7b885aa85c87130a50ddcc4
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-26 09:19:12 +0200

    :lipstick: MATH-25 add padding on math problem header for mobile devices

--- apps/frontend/src/components/ui/maths/MathHeader.tsx
+++ apps/frontend/src/components/ui/maths/MathHeader.tsx
@@ -2,7 +2,7 @@ import { Button } from "../button";
 
 export default function MathHeader() {
   return (
-    <div className="flex flex-1 font-medium justify-center lg:justify-start flex-row w-full h-full align-center bg-purple-500 header">
+    <div className="flex flex-1 font-medium justify-center lg:justify-start flex-row w-full h-full align-center bg-purple-500 header p-1 md:p-0">
       <section className="flex absolute w-full justify-start pl-3 self-center items-end">
         <a href="/">
           <Button
