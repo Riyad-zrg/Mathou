4bebf2d6d400f10e90a24c70a2a2ca18e8c7a27a
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-29 15:28:38 +0200

    :bug: MATH-26 modify dockerfile based on railway docs

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
 const nextConfig: NextConfig = {
   output: "standalone",
-  turbopack: { root: path.join(__dirname) },
 };
 
 export default nextConfig;
