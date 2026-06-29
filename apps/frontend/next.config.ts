24b0ffbb21157ee119dc023ea207c7b77c3fa94d
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-29 11:36:48 +0200

    :bug: MATH-26 fix turbopack for prod

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import path from "path";
 const nextConfig: NextConfig = {
   output: "standalone",
-  turbopack: { root: "." },
+  turbopack: { root: path.join(__dirname) },
 };
 
 export default nextConfig;
