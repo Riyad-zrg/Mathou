e98250d82b90750cb8b34b60c13defee6ec247b2
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-29 20:06:33 +0200

    :bug: MATH-26 modify dockerfile and main for prod

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,9 +5,9 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({origin:'http://localhost:3000', credentials:true});
-  await app.listen(process.env.PORT ?? 4000);
+  app.enableCors({ origin: process.env.CLIENT_URL, credentials: true });
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
 }
 bootstrap();
