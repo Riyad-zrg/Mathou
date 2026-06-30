0456c0b7c1ebae140f78e126d39d77206bbe5924
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-30 09:59:34 +0200

    :bug: MATH-26 fix turbopack issue

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
+  turbopack: process.env.NODE_ENV === "development" ? { root: "." } : undefined,
 };
 
 export default nextConfig;
