38db1f63323c237d6f57318606d5498096ac3a66
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-06-30 10:22:51 +0200

    :bug: MATH-26 add NEXT_PUBLIC_ prefix on env variable and clean dockerfile

--- apps/frontend/src/services/auth.service.ts
+++ apps/frontend/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { toast } from "sonner";
 
-const serverUrl = process.env.SERVER_URL;
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:4000";
 
 export async function fetchAccessToken(data: any) {
   let isError = false;
