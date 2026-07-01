d57e00c58c9a9b579172f688d6a3fec9a210ab49
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-07-01 11:40:10 +0200

    🏗️ MATH-26 modify structure to fix issue url param on client pages on prod

--- apps/frontend/src/app/signup/verify/page.tsx
+++ apps/frontend/src/app/signup/verify/page.tsx
@@ -1,54 +1,10 @@
-"use client";
-import { Alert, AlertDescription } from "@/src/components/ui/alert";
-import { Button } from "@/src/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/src/components/ui/card";
-import { getJWTpayload } from "@/src/lib/utils";
-import { verifyEmail } from "@/src/services/auth.service";
-import { AlertCircleIcon } from "lucide-react";
-import { useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import VerifyEmailPageComponent from "./verifyEmailPageComponent";
 
-export default function verifyEmailPage() {
-  const [error, setError] = useState(null);
-  const searchParams = useSearchParams();
-
-  const pathname = usePathname();
-
-  const verification_token = searchParams.get("verification_token");
-
-  console.log(pathname);
-  useEffect(() => {
-    const verify = async () => {
-      if (verification_token) {
-        const jwtPayload = await getJWTpayload(verification_token);
-        const result = await verifyEmail(verification_token, jwtPayload.email);
-        setError(result);
-      } else {
-        throw Error("Token invalide");
-      }
-    };
-    verify();
-  }, []);
-
-  if (error) {
-    return (
-      <div className="flex flex-col w-full h-100 place-items-center p-8 gap-8">
-        <Card className="flex flex-col w-8/10 h-55 justify-center align-middle text-base">
-          <CardContent>
-            <Alert variant="destructive" className="w-full">
-              <AlertCircleIcon />
-              <AlertDescription>{error}</AlertDescription>
-            </Alert>
-          </CardContent>
-          <CardFooter>
-            <a href="/" className="w-full">
-              <Button className="w-full">Retourner à l'accueil</Button>
-            </a>
-          </CardFooter>
-        </Card>
-      </div>
-    );
-  }
-
-  return <p>Login...</p>;
+export default async function verifyEmailPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const verification_token = (await searchParams).verification_token;
+  return <VerifyEmailPageComponent verification_token={verification_token} />;
 }
---
+++ apps/frontend/src/app/signup/verify/verifyEmailPageComponent.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Alert, AlertDescription } from "@/src/components/ui/alert";
+import { Button } from "@/src/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/src/components/ui/card";
+import { getJWTpayload } from "@/src/lib/utils";
+import { verifyEmail } from "@/src/services/auth.service";
+import { AlertCircleIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function VerifyEmailPageComponent({
+  verification_token,
+}: {
+  verification_token: string | string[] | undefined;
+}) {
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const verify = async () => {
+      if (verification_token) {
+        const jwtPayload = await getJWTpayload(verification_token);
+        const result = await verifyEmail(verification_token, jwtPayload.email);
+        setError(result);
+      } else {
+        throw Error("Token invalide");
+      }
+    };
+    verify();
+  }, []);
+
+  if (error) {
+    return (
+      <div className="flex flex-col w-full h-100 place-items-center p-8 gap-8">
+        <Card className="flex flex-col w-8/10 h-55 justify-center align-middle text-base">
+          <CardContent>
+            <Alert variant="destructive" className="w-full">
+              <AlertCircleIcon />
+              <AlertDescription>{error}</AlertDescription>
+            </Alert>
+          </CardContent>
+          <CardFooter>
+            <a href="/" className="w-full">
+              <Button className="w-full">Retourner à l'accueil</Button>
+            </a>
+          </CardFooter>
+        </Card>
+      </div>
+    );
+  }
+
+  return <p>Login...</p>;
+}
