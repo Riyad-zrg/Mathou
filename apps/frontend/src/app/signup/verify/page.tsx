87aad08f297769f5f8ba719c3d2e23a1cd7dcbeb
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-07-01 11:45:24 +0200

    :bug: MATH-26 make verify email page signup dynamic to access query parameters

--- apps/frontend/src/app/signup/verify/page.tsx
+++ apps/frontend/src/app/signup/verify/page.tsx
@@ -1,5 +1,7 @@
 import VerifyEmailPageComponent from "./verifyEmailPageComponent";
 
+export const dynamic = "force-dynamic";
+
 export default async function verifyEmailPage({
   searchParams,
 }: {
