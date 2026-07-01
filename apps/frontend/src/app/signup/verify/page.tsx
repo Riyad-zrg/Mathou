07e554f621055251161f47477ba39ed47e6975f9
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-07-01 15:04:27 +0200

    :bug: MATH-26 remove usePathname from client component

--- apps/frontend/src/app/signup/verify/page.tsx
+++ apps/frontend/src/app/signup/verify/page.tsx
@@ -1,15 +1,11 @@
-import path from "path";
 import VerifyEmailPageComponent from "./verifyEmailPageComponent";
-import { usePathname } from "next/navigation";
 export default async function verifyEmailPage({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const pathname = usePathname();
   const verification_token = (await searchParams).verification_token;
 
-  console.log("pathname", pathname);
   console.log("verifToken", verification_token);
   return <VerifyEmailPageComponent verification_token={verification_token} />;
 }
