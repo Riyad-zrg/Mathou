840e63f0e077a54a830639a318c6ec42dbe214ae
Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-07-01 14:44:37 +0200

    🔊 MATH-26 add logs on verify email page

--- apps/frontend/src/app/signup/verify/page.tsx
+++ apps/frontend/src/app/signup/verify/page.tsx
@@ -1,12 +1,15 @@
+import path from "path";
 import VerifyEmailPageComponent from "./verifyEmailPageComponent";
-
-export const dynamic = "force-dynamic";
-
+import { usePathname } from "next/navigation";
 export default async function verifyEmailPage({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
+  const pathname = usePathname();
   const verification_token = (await searchParams).verification_token;
+
+  console.log("pathname", pathname);
+  console.log("verifToken", verification_token);
   return <VerifyEmailPageComponent verification_token={verification_token} />;
 }
